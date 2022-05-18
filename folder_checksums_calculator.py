7d95abcfe2dbf7d5f24e5f8dc9e447d85c616a79
artv1 <105674496+artv1@users.noreply.github.com>
2022-05-18 11:32:39 +0300

    Update folder_checksums_calculator.py
    Added mode of checking existing files for compliance with the database

--- folder_checksums_calculator.py
+++ folder_checksums_calculator.py
@@ -1,8 +1,10 @@
+# https://github.com/artv1/folder_checksums_calculator
+
 import os, hashlib, sys
 from datetime import datetime
 
 #sha1 calculating
-def hash_calc(filename):
+def sha1_calc(filename):
     with open(filename, "rb") as f:
         file_hash = hashlib.sha1()
         while chunk := f.read(65536):
@@ -26,7 +28,71 @@ else:
     # if no argument detected
     dir = input("Enter folder: ") #getting the path to the working folder
 
-folder_name = os.path.basename(dir) #when need to use only the folder name instead of the full path
+# removing the '/' at the end of the path string, if present; to bring it to a single form
+if dir != '' and dir[-1]=='/':
+    dir = dir[:-1]
+
+'''
+Check if the specified [dir] is the path to the database file.
+If so, then the program works in the mode of checking existing files for compliance with the database.
+If not, the program skips this module and works in the mode of calculating checksums in the specified folder
+'''
+
+if os.path.isfile (dir) == True and dir.startswith(os.path.basename(dir)) == dir.startswith("checksums_list_for_"):
+    print (f"{50*'-'}\nYou've selected a DATABASE file: {os.path.basename(dir)}\nLet's begin verification\n{50*'-'}\n")
+
+    # now 'dir' is path to database file, so open it
+    dfile = open(dir)
+
+    # both options may be used
+    #check_dir = os.path.dirname(os.path.abspath(dir))+'/'
+    check_dir = dir.replace(os.path.basename(dir), '')
+
+    d_content = dfile.readlines()
+    raw_data = d_content[4:]
+
+    new_list = [s.replace("\n", "") for s in raw_data]
+    clear_list = list(filter(None, new_list)) # list of files and hashsums only
+
+    good_files = list() # files that have been verified against database
+    bad_files = list() # files that have NOT been verified against database
+
+    for cheking_file in clear_list:
+        if os.path.isfile(check_dir + cheking_file) == True:
+            
+            old_sha = clear_list[clear_list.index(cheking_file)+1] # next element is old sha1 from database
+            ch_sum = sha1_calc(check_dir + cheking_file)
+
+            print(f"Calculating SHA1 for {int(clear_list.index(cheking_file)/2+1)} of {int(len(clear_list)/2)} files in the list")
+
+            if old_sha == ch_sum:
+                good_files += [cheking_file]
+                print(f"{cheking_file}: file checked, checksums matched\nSHA1: {ch_sum}\n")
+            else:
+                bad_files += [cheking_file]
+                print(f"File: {cheking_file} was changed or CORRUPTED!!!\nCalculated SHA1: {ch_sum} but in database for this file stored SHA1: {old_sha}\n")
+
+    
+    # number of files that are in the list but not in the folder
+    number_of_lost_files = int(len(clear_list)/2)-(len(good_files)+len(bad_files))
+
+    print("Total summary:")
+    print(17*"-")
+    print (f"{len(good_files)} file(s) passed verification test\n{len(bad_files)} file(s) did NOT pass verification\n{number_of_lost_files} file(s) not found in the folder\n")
+
+    if len(bad_files) > 0:
+        print ("Checksums do not match for these files:")
+        for b_f in bad_files:
+            print(b_f)
+
+    exit()
+
+'''
+If the specified [dir] is NOT a path to a database file, the code below will run
+Works in checksums calculation mode in the specified folder
+'''
+
+folder_name = os.path.basename(dir) # when need to use only the folder name instead of the full path
 
 if dir == '':
     dir = os.path.abspath(os.getcwd())
@@ -35,7 +101,7 @@ if dir == '':
 if os.path.isdir (dir) == False:
     #if the user entered a file path instead of a folder path, compute a checksum for that file
     if os.path.isfile (dir) == True:
-        print (f"\n{dir} is a file, NOT a folder!\nCalculating checksum for it.\n\nSHA1: {hash_calc(dir)}")
+        print (f"\n{dir} is a file, NOT a folder!\nCalculating checksum for it.\n\nSHA1: {sha1_calc(dir)}")
     else:
         print ("Entered path does not exist.")
     exit()
@@ -69,9 +135,9 @@ database.write (f"Checksums calculated for {len(files)} files with a total size 
 for current_file in files:
     displayed_filename = current_file.replace(dir, '') #so that not the entire path to the file is stored in the database, but relative to the specified directory
     print (f"Calculating SHA1 for {files.index(current_file)+1} of {len(files)} files: {displayed_filename}")
-    sum = hash_calc(current_file) + '\n'
-    print (sum)
-    database.write (displayed_filename + '\n' + sum + '\n')
+    sha1_sum = sha1_calc(current_file) + '\n'
+    print (sha1_sum)
+    database.write (displayed_filename + '\n' + sha1_sum + '\n')
 
 database.close
 
