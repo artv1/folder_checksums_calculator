b4c2fe998ef7f83a9000fcc32ab6c22a57948b82
artv1 <temavlasa@gmail.com>
2022-05-26 17:02:38 +0300

    updated

--- folder_checksums_calculator.py
+++ folder_checksums_calculator.py
@@ -196,8 +196,11 @@ path = os.path.join(path,'')
 database_ram = list()
 
 print(f"""{50*'-'}
-The program will calculate SHA-1/256/512 checksums for all files in this folder as well as its subfolders.
+There are {len(files)} files with a total size of {converting_bytes(folder_size)} in the folder and its subfolders.
+
+The program will calculate SHA-1/256/512 checksums for all of them.
 A list of all files and their checksums will be saved and can be used to check data integrity.
+
 SHA-1 is chosen by default due to its superior computational speed.
 SHA-256 and SHA-512 are more reliable, but their computation is slower.
 {50*'*'}
